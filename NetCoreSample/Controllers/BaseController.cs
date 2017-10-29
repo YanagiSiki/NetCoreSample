32397b0cef11efd9b84bbb468594dcc4cda4bdeb
Yanagi <ce678013@gmail.com>
2017-10-29 17:26:49 +0800

    add BaseController add tool

---
+++ NetCoreSample/Controllers/BaseController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreSample.Models;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace NetCoreSample.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected static MongodbRepository _mongodbRepository = new MongodbRepository();
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var x = context.ActionDescriptor.AttributeRouteInfo;
+            if (!context.ModelState.IsValid)
+            {
+                throw new SampleException(context.ModelState);
+            }
+
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (filterContext.Exception != null)
+            {
+                Console.WriteLine(filterContext.Exception);
+                filterContext.ExceptionHandled = true;
+
+                ViewBag.ss = "12";
+                var controller = filterContext.Controller as Controller;
+
+                controller.ViewBag.Exception = "Foo message";
+
+                filterContext.Result = controller.View();
+
+                //filterContext.Result = new ViewResult
+                //{
+                //    ViewName = Url.Action((string)filterContext.RouteData.Values["Controller"], (string)filterContext.RouteData.Values["action"]),
+                //    ViewData =
+                //    //{ {"Exception", filterContext.Exception } },
+                //};
+            }
+
+        }
+    }
+}
---
+++ NetCoreSample/Helper/ConfigurationHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreSample.Helper
+{
+    public class ConfigurationHelper
+    {
+        static IConfiguration _configuration;
+        public ConfigurationHelper()
+        {
+            _configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+        }
+
+        public string GetValue(string key)
+        {
+            return _configuration.GetSection(key).Value.ToString();
+        }
+    }
+}
