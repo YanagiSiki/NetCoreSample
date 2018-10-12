31b2b23d8a6e74cf7089c9f06e0907a0ce4d1018
Yanagi <ce678013@gmail.com>
2018-10-13 01:05:20 +0800

    add

--- NetCoreSample/Controllers/WebApi/UserController.cs
+++ NetCoreSample/Controllers/WebApi/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NetCoreSample.Models;
 using NetCoreSample.Tools;
 
@@ -10,12 +11,12 @@ namespace NetCoreSample.Controllers.WebApi
 {
     [Route("UserApi/[action]")]
     [AllowAnonymous]
-    public class UserController: Controller
+    public class UserController : Controller
     {
         private NpgsqlContext _npgsql;
         public UserController(NpgsqlContext npgsql)
         {
-            if(_npgsql == null) _npgsql = npgsql;
+            if (_npgsql == null)_npgsql = npgsql;
         }
 
         [HttpGet]
@@ -26,12 +27,41 @@ namespace NetCoreSample.Controllers.WebApi
         }
 
         [HttpPost]
-        public IActionResult AddUser(User user)
+        public IActionResult AddUser()
         {
-            _npgsql.User.Where(u => (user.Name.IsNullOrEmpty() || u.Name == user.Name));
+            var user = new User()
+            {
+                Email = $"{StringTool.GenerateString(8)}@gmail.com",
+                Name = StringTool.GenerateString(5),
+                Password = "p@ssWord"
+
+            };
+            _npgsql.User.Add(user);
+            _npgsql.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult GetPostOfUser(int userId = 0)
+        {
+            if (_npgsql.User.All(_ => _.UserId != userId))
+                return Ok("User Not Found");
+
+            var user = _npgsql.User.Include("Posts.PostTags.Tag").SingleOrDefault(_ => _.UserId == userId);
             return Ok(user);
         }
 
-        
+        [HttpPost]
+        public IActionResult GetTagOfUser(int userId = 0)
+        {
+            if (_npgsql.User.All(_ => _.UserId != userId))
+                return Ok("User Not Found");
+
+            var tags = _npgsql.User.Include("Posts.PostTags.Tag").Where(_ => _.UserId == userId)
+                .SelectMany(tl => tl.Posts.SelectMany(p => p.PostTags.Select(pt => pt.Tag)))
+                .Distinct().ToList();
+
+            return Ok(tags);
+        }
     }
 }
