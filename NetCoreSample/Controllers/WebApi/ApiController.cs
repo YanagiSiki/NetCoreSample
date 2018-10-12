676edd7336697e691c29c018a9ce9b9f1c5d216c
Yanagi <ce678013@gmail.com>
2018-10-12 20:26:45 +0800

    Add api 升版至net core 2.1

--- NetCoreSample/Controllers/WebApi/ApiController.cs
+++ NetCoreSample/Controllers/WebApi/ApiController.cs
@@ -18,6 +18,7 @@ namespace NetCoreSample.Controllers.WebApi
             if (_npgsql == null)_npgsql = npgsql;
         }
 
+        [HttpPost]
         public IActionResult InsertPost()
         {
             var users = _npgsql.User.ToList();
@@ -37,6 +38,38 @@ namespace NetCoreSample.Controllers.WebApi
             return Ok();
         }
 
+        [HttpPost]
+        public IActionResult InsertTag()
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                var tag = new Tag() { TagName = StringTool.GenerateString(3) };
+                _npgsql.Tag.Add(tag);
+            }
+            _npgsql.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult InsertPostTag()
+        {
+            var posts = _npgsql.Post.ToList();
+            var tags = _npgsql.Tag.ToList();
+            var PostTags = _npgsql.PostTag.ToList();
+
+            for (var i = 0; i < 3; i++)
+            {
+                var post = posts.GetRandomItem();
+                var tag = tags.GetRandomItem();
+                var postTag = new PostTag() { PostId = post.PostId, TagId = tag.TagId };
+                if (PostTags.Any(_ => _.PostId == postTag.PostId && _.TagId == postTag.TagId))
+                    continue;
+                _npgsql.PostTag.Add(postTag);
+            }
+
+            _npgsql.SaveChanges();
+            return Ok();
+        }
     }
 
 }
