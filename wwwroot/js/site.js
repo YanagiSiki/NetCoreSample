02d9d874089645395cad3eda76b776cbe4d7de6f
YanagiSiki <ce678013@gmail.com>
2019-04-26 16:03:10 +0800

    fix the gad damn css

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,13 +1,13 @@
 (function (w) {
     //side bar toggle
     $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').removeClass('noAction');
         $('#sidebar').toggleClass('active');
+        $('#content').toggleClass('active');
         localStorage.setItem("isToggle", String($('#sidebar').is('.active')));
     });
     //record side bar toggle state
     if (!!localStorage.getItem('isToggle') && localStorage.getItem('isToggle') !== String($('#sidebar').is('.active'))) {
-        $('#sidebar').addClass('noAction');
+        $('#content').toggleClass('active');
         $('#sidebar').toggleClass('active');
     }
 }(window));
