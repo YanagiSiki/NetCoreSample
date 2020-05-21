51dbf3052f11727ac9c0f58a2e46ef7f9d107b79
YanagiSiki <ce678013@gmail.com>
2020-05-21 15:32:05 +0800

    [bug] login頁面按下enter會自動submit

--- wwwroot/js/login.ts
+++ wwwroot/js/login.ts
@@ -2,6 +2,15 @@ import { tool } from "./tool.js";
 
 (function (w) {
 
+    // https://stackoverflow.com/questions/27807853/html5-how-to-make-a-form-submit-after-pressing-enter-at-any-of-the-text-inputs
+    // 這段自定義submit，無法按enter自動執行form submit，所以加上這段keypress
+    $('#form :text, :password').keypress(function(event) {
+        if (event.keyCode == 13 || event.which == 13) {
+            $('#submitbtn').click();
+            event.preventDefault();
+        }
+    });
+
     $('#submitbtn').click(() => {
         let submitUrl = '/UserApi/Login';
         let returnUrl = $('#returnUrl').val().toString();
