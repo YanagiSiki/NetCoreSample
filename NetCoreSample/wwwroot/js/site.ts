aad70eb5474df8f199845222d16242600c02d847
YanagiSiki <ce678013@gmail.com>
2020-07-08 08:59:01 +0800

    [bug] UI 1. 不記sidebar狀態了，開新視窗或跳頁時如果看到sidebar collapse會很煩

--- NetCoreSample/wwwroot/js/site.ts
+++ NetCoreSample/wwwroot/js/site.ts
@@ -3,13 +3,13 @@
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
         $('#content').toggleClass('active');
-        localStorage.setItem("isToggle", String($('#sidebar').is('.active')));
+        // localStorage.setItem("isToggle", String($('#sidebar').is('.active')));
     });
     //record side bar toggle state
-    if (!!localStorage.getItem('isToggle') && localStorage.getItem('isToggle') !== String($('#sidebar').is('.active'))) {
-        $('#content').toggleClass('active');
-        $('#sidebar').toggleClass('active');
-    }
+    // if (!!localStorage.getItem('isToggle') && localStorage.getItem('isToggle') !== String($('#sidebar').is('.active'))) {
+    //     $('#content').toggleClass('active');
+    //     $('#sidebar').toggleClass('active');
+    // }
     hljs.initHighlightingOnLoad();
     hljs.initLineNumbersOnLoad();
 }(window));
