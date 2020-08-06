f67223e2a9edac994c52a7fbed0801232e45f479
YanagiSiki <ce678013@gmail.com>
2020-08-06 12:00:05 +0800

    [feature] hangfire job display job name

--- NetCoreSample/BatchJob/BatchJob.cs
+++ NetCoreSample/BatchJob/BatchJob.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using Hangfire;
 using NetCoreSample.Tools;
@@ -11,15 +12,24 @@ namespace NetCoreSample.BatchJobs
         {
             var task = new Task<string>(() =>
             {
+                var userId = "Ya";
+                var jobName = "寫些東西到console";
                 for (var i = 1; i < 10000; i++)
                 {
                     var str = StringTool.GenerateString(5);
-                    BackgroundJob.Enqueue(() => Console.WriteLine($"Hello {str}"));
+                    BackgroundJob.Enqueue(() => ExecuteJob($"{userId} 建立於 {DateTime.Now:MM-dd HH:mm:ss}", jobName, str));
                     // RecurringJob.AddOrUpdate("流程A", () => Console.Write($"Hello {str}"), Cron.Minutely);
                 }
                 return "";
             });
             return task;
+        }
+
+        [DisplayName("排程作業: {0} - {1}")]
+        public static void ExecuteJob(string creattor, string jobName, string str)
+        {
+            Console.WriteLine($"Hello {str}");
+            return;
         }
     }
 }
