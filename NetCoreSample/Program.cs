b808f2c8850d65c2cbcd70748a2c6133309134e9
Yanagi <ce678013@gmail.com>
2022-04-07 14:40:05 +0800

    feat: 修改dockerfile for 3.1

--- NetCoreSample/Program.cs
+++ NetCoreSample/Program.cs
@@ -26,6 +26,7 @@ namespace NetCoreSample
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
+                webBuilder.UseUrls("http://0.0.0.0:5000");
             });
 
         // public static IWebHost BuildWebHost(string[] args) =>
