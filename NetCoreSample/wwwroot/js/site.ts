6bc05baf120f22d143fc5c4b6107c297536a26a0
Yanagi <ce678013@gmail.com>
2025-11-18 01:09:04 +0800

    fix

--- NetCoreSample/wwwroot/js/site.ts
+++ NetCoreSample/wwwroot/js/site.ts
@@ -1,7 +1,13 @@
-﻿(function (w) {
+﻿// hljs 為全域命名空間，已在 declaration.d.ts 宣告
+declare namespace hljs {
+    function initHighlightingOnLoad(): void;
+    function initLineNumbersOnLoad(): void;
+}
+(function (w) {
     //side bar toggle
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
+        $('#content').toggleClass('active');
         // localStorage.setItem("isToggle", String($('#sidebar').is('.active')));
     });
     //record side bar toggle state
